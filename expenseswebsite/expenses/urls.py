181c7bca45817250aa3c6c6e8988d338b2ff1d22
dhruv <dhruvprajapati152@gamail.com>
2025-01-03 10:33:58 +0530

    Start new Project

---
+++ expenseswebsite/expenses/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path('',views.index,name='expenses'),
+   path('add-expenses',views.add_expense,name='add-expenses')
+]
